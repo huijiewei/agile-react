70846574d0a315a159e21b9d74616e90f43ff598
Huijie Wei <huijiewei@outlook.com>
2021-02-08 15:24:47 +0800

    更新

--- src/app/admin/App.tsx
+++ src/app/admin/App.tsx
@@ -1,7 +1,4 @@
 import { Routes, Route, Navigate, BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'use-http';
-
-import { useError } from '@shared/hooks/useError';
 
 import Home from '@admin/views/site/Home';
 import About from '@admin/views/site/About';
@@ -15,95 +12,23 @@ import useSplash from '@shared/hooks/useSplash';
 
 import '@admin/assets/styles/admin.less';
 
-const UnauthorizedHttpCode = 401;
-const UnprocessableEntityHttpCode = 422;
-
-const HttpGetMethod: string[] = ['GET', 'HEAD'];
-
 const App = (): JSX.Element => {
-  const { addError } = useError();
-
   useSplash();
 
-  const handleError = (error) => {
-    const historyBack = error.config.historyBack;
-
-    console.log(error);
-
-    if (!error.response) {
-      addError(error.message, historyBack);
-
-      return;
-    }
-
-    if (error.response.status === UnauthorizedHttpCode) {
-      if (!error.config.__storeDispatch) {
-        error.config.__storeDispatch = true;
-
-        if (historyBack || HttpGetMethod.includes(error.config.method.toUpperCase())) {
-          // 跳转登录
-        } else {
-          // 窗口登录
-        }
-      }
-
-      return;
-    }
-
-    if (error.response.status === UnprocessableEntityHttpCode) {
-      return;
-    }
-
-    addError(
-      error.response.data.detail ||
-        error.response.data.message ||
-        error.response.data.title ||
-        error.response.statusText ||
-        '网络请求错误',
-      historyBack,
-    );
-  };
-
-  const options = {
-    interceptors: {
-      request: ({ options }) => {
-        console.log(options);
-        options.historyBack = options.historyBack || true;
-        console.log(options);
-        return options;
-      },
-      response: ({ response }) => {
-        console.log(response);
-
-        if (!response.ok) {
-          return handleError(response.data);
-        }
-
-        return response;
-      },
-    },
-    onError: ({ error }) => {
-      console.log(error.status);
-      handleError(error);
-    },
-  };
-
   return (
-    <Provider url={document.querySelector('meta[name="api-host"]').getAttribute('content')} options={options}>
-      <Router>
-        <Routes basename={process.env.PUBLIC_URL}>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<Navigate to="home" replace={true} />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-          <Route element={<BlankLayout />}>
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <Routes basename={process.env.PUBLIC_URL}>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Navigate to="home" replace={true} />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+        <Route element={<BlankLayout />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
