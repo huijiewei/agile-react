0420bc4d18b5acbee226cd2364dc75ecbb1b1b7d
Huijie Wei <huijiewei@outlook.com>
2021-03-01 04:33:12 +0800

    更新

--- src/app/admin/layouts/DefaultLayout.tsx
+++ src/app/admin/layouts/DefaultLayout.tsx
@@ -1,42 +1,43 @@
 import { Navigate, NavLink, Outlet } from 'react-router-dom';
-import { FC, Suspense, VFC } from 'react';
+import { Suspense, useState, VFC } from 'react';
 import { AuthLoginAction, useAuthLoginState } from '@shared/contexts/AuthLoginContext';
-import { IMenu, useAuthUserState } from '@admin/contexts/AuthUserContext';
-import { AppBar, Container, Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar } from '@material-ui/core';
+import { useAuthUserState } from '@admin/contexts/AuthUserContext';
+import {
+  AppBar,
+  Collapse,
+  Container,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from '@material-ui/core';
 import useRefreshAuthUser from '@admin/hooks/useRefreshAuthUser';
 import { formatUrl } from '@shared/utils/utils';
+import { ExpandLess, ExpandMore } from '@material-ui/icons';
 
 const drawerWidth = 220;
 
-const AgileHeader: VFC = () => {
+const AgileHead: VFC = () => {
   return (
-    <header className={'ag-header'}>
-      <nav className={'ag-nav'}>
-        <div className={'ag-left'}>1</div>
-      </nav>
-      <div className={'ag-tabs'}>
-        <ul>
-          <li>Tab 1</li>
-          <li>Tab 2</li>
-          <li>Tab 3</li>
-          <li>Tab 4</li>
-          <li>Tab 5</li>
-          <li>Tab 6</li>
-        </ul>
-      </div>
-    </header>
+    <AppBar color="transparent" position="fixed">
+      <Toolbar></Toolbar>
+    </AppBar>
   );
 };
 
 const AgileSideMenu: VFC = () => {
   const { menus } = useAuthUserState();
 
-  return <AgileSideNestMenu menus={menus} />;
+  return <AgileSideNestMenu keyPrefix="m-" component="nav" menus={menus} />;
 };
 
-const AgileSideMenuItem: VFC = ({ menu }) => {
+const AgileSideMenuItem: VFC = (props) => {
+  const { menu, hasChildren, onClick, open } = props;
+
   return (
-    <ListItem button>
+    <ListItem button onClick={onClick}>
       {menu.icon && (
         <ListItemIcon sx={{ minWidth: '1em', marginRight: '6px' }}>
           <svg
@@ -51,18 +52,37 @@ const AgileSideMenuItem: VFC = ({ menu }) => {
       <ListItemText disableTypography>
         {menu.url ? <NavLink to={formatUrl(menu.url)}>{menu.label}</NavLink> : menu.label}
       </ListItemText>
+      {hasChildren && (open ? <ExpandLess /> : <ExpandMore />)}
     </ListItem>
   );
 };
 
-const AgileSideNestMenu: VFC = ({ menus }) => {
+const AgileSideNestMenu: VFC = (props) => {
+  const { menus, component, keyPrefix } = props;
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    console.log(open);
+    setOpen(!open);
+  };
+
   return (
-    <List>
-      {menus.map((menu) => {
+    <List component={component}>
+      {menus.map((menu, index) => {
         return (
           <>
-            <AgileSideMenuItem menu={menu} />
-            {menu.children && <AgileSideNestMenu menus={menu.children} />}
+            <AgileSideMenuItem
+              open={open}
+              hasChildren={!!menu.children}
+              onClick={handleClick}
+              key={keyPrefix + index}
+              menu={menu}
+            />
+            {menu.children && (
+              <Collapse key={'c' + keyPrefix + index} timeout="auto" in={open} unmountOnExit>
+                <AgileSideNestMenu keyPrefix={keyPrefix + index} component="div" menus={menu.children} />
+              </Collapse>
+            )}
           </>
         );
       })}
@@ -72,7 +92,7 @@ const AgileSideNestMenu: VFC = ({ menus }) => {
 
 const AgileSide: VFC = () => {
   return (
-    <Drawer open={true} variant={'permanent'}>
+    <Drawer style={{ width: drawerWidth, flexShrink: 0 }} open variant="persistent">
       <div className={'ag-brand'}>
         <img alt="Agile" src={require('../assets/images/logo.png')} />
         <img alt="Boilerplate" src={require('../assets/images/banner-white.png')} />
@@ -94,12 +114,10 @@ const DefaultLayout: VFC = () => {
   return authLoginAction == AuthLoginAction.DIRECT ? (
     <Navigate to={'login'} replace={true} />
   ) : (
-    <Container disableGutters={true} maxWidth={false}>
-      <AppBar color="transparent" position="fixed">
-        <Toolbar></Toolbar>
-      </AppBar>
+    <Container style={{ display: 'flex' }} disableGutters={true} maxWidth={false}>
+      <AgileHead />
       <AgileSide />
-      <main>
+      <main style={{ flexGrow: 1 }}>
         <Suspense fallback={null}>
           <Outlet />
         </Suspense>
