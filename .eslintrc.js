fd9ed1e2f9ee36f8ac692964159327a66c0b25c6
Huijie Wei <huijiewei@outlook.com>
2021-04-22 00:10:12 +0800

    更新 ESLint 规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,14 +29,5 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
-  overrides: [
-    {
-      files: ['*.ts'],
-      rules: {
-        '@typescript-eslint/explicit-module-boundary-types': 'warn',
-      },
-    },
-  ],
 };
