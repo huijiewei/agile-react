ff2ac86e853e6ba8546f9f7a20e391cc9688419a
Huijie Wei <huijiewei@outlook.com>
2021-04-24 19:09:56 +0800

    Document title 更新

--- src/app/admin/components/NavBreadcrumb.tsx
+++ src/app/admin/components/NavBreadcrumb.tsx
@@ -3,9 +3,30 @@ import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, Text } from '@chakra-ui/rea
 import { Icon } from '@shared/components/icon/Icon';
 import { Home, Right } from '@icon-park/react';
 import { Link } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
 
 const NavBreadcrumb = (): JSX.Element => {
   const breadcrumbs = useBreadcrumb();
+
+  const defaultTitle = useRef(document.title);
+
+  useEffect(() => {
+    document.title =
+      breadcrumbs
+        .reverse()
+        .map((breadcrumb) => {
+          return breadcrumb.title;
+        })
+        .join(' - ') +
+      ' - ' +
+      defaultTitle.current;
+  }, [breadcrumbs]);
+
+  useEffect(() => {
+    return () => {
+      document.title = defaultTitle.current;
+    };
+  }, []);
 
   return (
     <Breadcrumb spacing={2} separator={<Icon verticalAlign="-3px" as={Right} />} paddingStart="3">
