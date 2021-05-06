4e32a65a6f13322d5b93e168ab246f4dc6f61c43
Huijie Wei <huijiewei@outlook.com>
2021-05-06 10:00:46 +0800

    Card 组件, 待优化

---
+++ src/shared/components/card/Card.tsx
@@ -0,0 +1,41 @@
+import { Box, BoxProps, forwardRef, Heading, HeadingProps } from '@chakra-ui/react';
+
+const CardHeader = (props: HeadingProps): JSX.Element => {
+  const { children, ...restProps } = props;
+
+  return (
+    <Heading
+      borderBottomColor={'gray.200'}
+      borderBottomWidth={'1px'}
+      backgroundColor={'gray.50'}
+      padding={3}
+      size={'sm'}
+      as={'h2'}
+      {...restProps}
+    >
+      {children}
+    </Heading>
+  );
+};
+
+const CardBody = (props: BoxProps): JSX.Element => {
+  const { children, ...restProps } = props;
+
+  return (
+    <Box padding={3} {...restProps}>
+      {children}
+    </Box>
+  );
+};
+
+const Card = (props: BoxProps): JSX.Element => {
+  const { children, ...restProps } = props;
+
+  return (
+    <Box shadow={'sm'} borderWidth={'1px'} borderColor={'gray.200'} borderRadius={'md'} {...restProps}>
+      {children}
+    </Box>
+  );
+};
+
+export { Card, CardHeader, CardBody };
