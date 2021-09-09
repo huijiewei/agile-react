cb89051a5331a358bac85489419d47f08a19f1af
Huijie Wei <huijiewei@outlook.com>
2021-09-09 09:53:06 +0800

    删除日志输出

--- src/app/admin/components/NavBreadcrumb.tsx
+++ src/app/admin/components/NavBreadcrumb.tsx
@@ -33,8 +33,6 @@ const NavBreadcrumb = (props: BreadcrumbProps): JSX.Element => {
       };
     });
 
-    console.log(breadcrumbs);
-
     setBreadcrumbs(breadcrumbs);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
